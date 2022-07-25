ad000a3dcf080aa429e952257281a0d28d90d84c
theflyer1983 <theflyer1983@gmail.com>
2022-07-25 15:24:05 +0100

    added comments

--- src/components/MuseumHighlight/MuseumHighlight.vue
+++ src/components/MuseumHighlight/MuseumHighlight.vue
@@ -4,12 +4,13 @@ import StarIcon from '@/assets/StarIcon.vue'
 import { Highlight } from '@/types'
 import { Routes } from '@/router/routes';
 
+// Typescript way of defining props in <script setup>: https://vuejs.org/api/sfc-script-setup.html#typescript-only-features
 const props = defineProps<{
   highlight: Highlight,
   page: string,
 }>();
 
-const newsDate = computed(() => props.highlight.news?.date)
+const newsDate = computed(() => props.highlight.news?.date) //Didn't know where to use this as it only appeared on one hightlight, but if it is there then this computed property will return that date.
 
 const icon = computed(() => {
   if (props.page === Routes.Space) {
@@ -26,7 +27,7 @@ const isExternal = computed(() => {
 </script>
 
 <template>
-    <div class="museum-highlight" :class="{ 'external': isExternal }">
+    <div class="museum-highlight" :class="{ 'external': isExternal }"> <!-- The binding for the class here is to check the computed property and then apply a dynamic class regarding if this is from the main API or the Partner API-->
         <!-- Display the available information for the highlight -->
         <div class="museum-highlight__image">
           <!-- Dynamic component to show highlight badge dependant on page -->
@@ -38,7 +39,7 @@ const isExternal = computed(() => {
           {{highlight.name}}
         </div>
         <div>
-          {{highlight.description || highlight.info}}
+          {{highlight.description || highlight.info}} <!-- Because the description is named info from the parter, then this is showing one or the other, depending on which one is available. This could also be done in a computed property.-->
         </div>
     </div>
 </template>
